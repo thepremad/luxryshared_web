7bcf0c43f787a99cd7062c9082b921ce8d3ffde8
Deepak Goswami <deepakpremad@gmail.com>
2024-06-15 11:00:44 +0530

    changes in add item api

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -18,7 +18,7 @@
                         class="badge badge-light-warning rounded-pill ms-auto me-1"></span></a>
 
             </li>
-             <!-- <li
+             <li
                 class=" nav-item {{ Request::routeIs('admin.categories.index', 'admin.categories.create', 'admin.categories.show', 'admin.categories.edit', 'admin.categories.appraisals', 'admin.categories.loans', 'admin.categories.salaries') ? 'has-sub open' : '' }} ">
                 <a class="d-flex align-items-center" href="#"><i class="fa-solid fa-list"></i><span
                         class="menu-title text-truncate" data-i18n="Invoice">Category Master</span></a>
@@ -80,7 +80,7 @@
 
 
                 </ul>
-            </li> -->
+            </li>
             <li
                 class=" nav-item {{ Request::routeIs('admin.user.register_request', 'admin.user.index', 'admin.user.edit') ? 'has-sub open' : '' }} ">
                 <a class="d-flex align-items-center" href="#"><i class="fa-solid fa-user-injured"></i><span
@@ -123,7 +123,7 @@
 
                 </ul>
             </li>
-            <!-- <li
+            <li
                 class=" nav-item {{ Request::routeIs('admin.products.register_request', 'admin.products.index', 'admin.products.edit') ? 'has-sub open' : '' }} ">
                 <a class="d-flex align-items-center" href="#"><i class="fa-brands fa-product-hunt"></i><span
                         class="menu-title text-truncate" data-i18n="Invoice">Product Listing</span></a>
@@ -324,7 +324,7 @@
                     </li>
 
                 </ul>
-            </li>  -->
+            </li> 
 
 
         </ul>
