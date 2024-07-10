a7e82c03e029897c8289d7443e9ce6b9c1b636f7
FIRST_NAME LAST_NAME <govindsinghpremad@gmail.com>
2024-07-10 13:07:33 +0530

    style changes#1

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -7,8 +7,8 @@
     <title>Lxry Shared</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{asset('luxury-shared/public/frontend/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('luxury-shared/public/frontend/css/responsive.css')}}">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <!-- Owl Carousel CSS CDN -->
