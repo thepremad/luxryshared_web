9036e71e7b6239e0b985021a0f8bac8cdc0cf9f5
deepakgoswami <124884739+Deepak10225@users.noreply.github.com>
2024-07-30 12:16:03 +0530

    Update RentProductResource.php

--- app/Http/Resources/RentProductResource.php
+++ app/Http/Resources/RentProductResource.php
@@ -15,18 +15,21 @@ class RentProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'=> $this->products->id,
-            'icon' => $this->products->mainImag ? url('public/uploads/item/'.$this->products->mainImag) : null,
-            'productName' => $this->products->item_title,
-            'price' => $this->product_price,
-            'ownerName' => $this->products->users->first_name ?? ''.' '.$this->products->users->last_name ?? '',
-            'rentorName' => auth()->user()->first_name.' '.auth()->user()->last_name,
-            'RentorMobNumber' => auth()->user()->number,
-            'paymentDetails' =>$this->product_price,
-            'bookingDate' => date_format($this->created_at,"d F Y"),
-            'quantity_of_product' => 1,
-            'rentalEmail' => auth()->user()->email
-            
-        ];
+                'id' => $this->products->id ?? null,
+                'icon' => $this->products?->mainImag 
+                ? url('public/uploads/item/'.$this->products?->mainImag) 
+                : null,
+                'productName' => $this->products->item_title ?? 'N/A',
+                'price' => $this->product_price ?? 0,
+                'ownerName' => ($this->products->users->first_name ?? '') . ' ' . ($this->products->users->last_name ?? ''),
+                'rentorName' => (auth()->user()->first_name ?? '') . ' ' . (auth()->user()->last_name ?? ''),
+                'RentorMobNumber' => auth()->user()->number ?? 'N/A',
+                'paymentDetails' => $this->product_price ?? 0,
+                'bookingDate' => $this->created_at 
+                    ? $this->created_at->format('d F Y') 
+                    : 'N/A',
+                'quantity_of_product' => 1,
+                'rentalEmail' => auth()->user()->email ?? 'N/A'
+            ]
     }
 }
