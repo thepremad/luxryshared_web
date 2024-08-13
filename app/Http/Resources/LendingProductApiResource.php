345dd3df14154631f3e1c4f121f58f70b94e88dd
deepakgoswami <124884739+Deepak10225@users.noreply.github.com>
2024-08-13 19:06:37 +0530

    Update LendingProductApiResource.php

--- app/Http/Resources/LendingProductApiResource.php
+++ app/Http/Resources/LendingProductApiResource.php
@@ -26,7 +26,7 @@ class LendingProductApiResource extends JsonResource
             'RentorMobNumber' => $this->user->number,
             'paymentDetails' => $this->products->rrp_price ?? '',
             'bookingDate' => date_format($this->created_at,'d-m-Y') ?? '',
-            'rentalPeriod' => $this->bookingdate->date ?? '',
+            // 'rentalPeriod' => $this->bookingdate->date ?? '',
             'quantity_of_product' => 1,
             'rentalEmail' => $this->user->email,
             'final_amount' => $this->products->rrp_price ?? '',
