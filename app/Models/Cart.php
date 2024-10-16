9afef46ce6d2418e7fb2eb280513074c5b3d0512
kapilpremad <kapilpremad90@gmail.com>
2024-10-16 12:03:41 +0530

    update the add to cart api

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,6 +9,8 @@ class Cart extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['item_id','user_id','days','type','rent_from','rent_to'];
+
     public function products(){
         return $this->belongsTo(Item::class,'item_id','id');
     }
