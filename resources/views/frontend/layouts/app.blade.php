8d1b259f2851faf9588bcf66831bfea5ad45f27e
Deepak10225 <deepakpremad@gmail.com>
2024-10-29 23:08:16 +0530

    list in item Fix step 3

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style2.css') }}">
     {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     
     <!-- Slider Link -->
